b091a50c304e25a74d2ab72bc7d3608a40426468
TanmayPShinde <tps.shinde@gmail.com>
2023-03-31 12:52:36 +0530

    feat: cardProcess component

---
+++ src/components/shared/card/cardProcess/CardProcess.jsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+import P from "../../p";
+import { Link } from "react-router-dom";
+
+const CardProcess = ({ icon, heading, children, link }) => {
+  return (
+    <div className="card-process">
+      <div className="process-icon">
+        <img src={icon} loading="lazy" />
+      </div>
+      <div className="heading-large">{heading}</div>
+      <P variant="paragraph-standard">{children}</P>
+      <Link className="butt" to={link}>
+        <div className="butt-text">Learn more</div>
+      </Link>
+    </div>
+  );
+};
+
+export default CardProcess;
--- src/pages/home/Description.jsx
+++ src/pages/home/Description.jsx
@@ -3,6 +3,10 @@ import ContainerLarge from "../../components/shared/container/containerLarge/Con
 import Wrap from "../../components/shared/wrap";
 import P from "../../components/shared/p";
 import Heading from "../../components/shared/heading";
+import CardProcess from "../../components/shared/card/cardProcess/CardProcess";
+import EmployersIcon from "./assets/group-11375.svg";
+import UniversityIcon from "./assets/group-11374.svg";
+import CandidatesIcon from "./assets/group-11373.svg";
 
 const Description = () => {
   return (
@@ -13,6 +17,38 @@ const Description = () => {
             <P variant="title">OUR&nbsp;SERVICES</P>
             <Heading>End - to - End solution for Hiring and Placement</Heading>
           </Wrap>
+          <div className="row gx-4 gy-4">
+            <div className="col-lg-4 col-md-6">
+              <CardProcess
+                heading="For Employers"
+                link="/for-employers"
+                icon={EmployersIcon}
+              >
+                Campus-Hiring and online <br />
+                Recruitment Infrastructure.
+              </CardProcess>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <CardProcess
+                heading="For Universities"
+                link="/for-universities"
+                icon={UniversityIcon}
+              >
+                Automated and networked <br />
+                Placement System.
+              </CardProcess>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <CardProcess
+                heading="For Candidates"
+                link="/for-candidates"
+                icon={CandidatesIcon}
+              >
+                Job application tracking <br />
+                and &nbsp;profile building.
+              </CardProcess>
+            </div>
+          </div>
         </ContainerLarge>
       </Section>
     </Section>
